b9744cdf1a1997dc6f64c946b66c3ed45aea5906
jose hernandez <josegho2013@gmail.com>
2022-05-25 13:07:40 -0300

    update

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-// import { getAllPersonajes, getPersonaje, createPersonaje, updatePersonaje, deletePersonaje } from '../resolver/AnimeResolver.js'
+ //import { getAllPersonajes, getPersonaje, createPersonaje, updatePersonaje, deletePersonaje } from '../resolver/AnimeResolver.js'
 import { getAllPersonajes } from '../resolver/AnimeResolver.js'
 const router = express.Router()
 
-router.get('/',  getAllPersonajes)
+// router.get('/',  getAllPersonajes)
 // router.get('/:id', getPersonaje)
 // router.post('/', createPersonaje)
 // router.put('/:id', updatePersonaje)
